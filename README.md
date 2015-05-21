2279bd64e0ea28276bd1a99ed87255ed46fe171c
Brantone <brenton@letrabb.com>
2015-05-21 12:58:43 -0700

    Added test script and updated instructions

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # unity-delete-script-bug
 To prove deleting a file on batchmode build causes compilation problems.
 
-This has been tested using Unity 5.0.0f4 .. but I'm told it affects other versions of Unity 5 as well.
+This has been tested on OSX with Unity 5.0.0f4.
 
-Initial project setup was done with basic new project, single folder, couple scripts, nothing fancy.
-The project is set to "Visible Meta Files" for Version Control Mode; and "Force Text" for Serialization.
+Project is basic new project, single folder, couple scripts, nothing fancy.
+It is set to "Visible Meta Files" for Version Control Mode; and "Force Text" for Serialization.
 The build scripts were created for full blown projects, based on online docs and general needs.
 
-
+## Usage:
+Open run_test_case.sh, verify UNITY_CMD_PATH and observe commands to understand what's happening.
+Run:
+`bash run_test_case.sh`
---
+++ run_test_case.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Running test
+
+# Path to Unity command
+UNITY_CMD_PATH=/Applications/Unity_v500f4/Unity.app/Contents/MacOS/Unity
+UNITY_PROJECT_PATH=$(pwd)/test-project/
+UNITY_EDITOR_LOG=~/Library/Logs/Unity/Editor.log
+OUTPUT_DIR=$(pwd)/_output/
+
+# Start clean
+rm -rf $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
+
+echo "1. Run first Unity build ... this should complete successfully"
+$UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -phase=CI -outputDir=$OUTPUT_DIR/build_step_1/
+echo "      Result: " $?
+cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_1.log
+
+
+echo "2. Remove a script file and it's corresponding meta file"
+mkdir -p $(pwd)/tmp_deleted_file/
+mv $UNITY_PROJECT_PATH/Assets/MyFolder/TestScript_toDelete.cs* $(pwd)/tmp_deleted_file/ 
+
+
+echo "3. Run Unity build again ... this should fail"
+$UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -phase=CI -outputDir=$OUTPUT_DIR/build_step_3/
+echo "      Result: " $?
+echo "      Open _output/unity_editor_step_3.log and search for 'stderr'"
+cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_3.log
+
+
+echo "4. Run Unity build again ... this should complete successfully"
+$UNITY_CMD_PATH -quit -batchmode -projectPath $UNITY_PROJECT_PATH -buildTarget iPhone -executeMethod Build.ClientBuilder.BuildClient_BatchMode -phase=CI -outputDir=$OUTPUT_DIR/build_step_4/
+echo "      Result: " $?
+cp  ~/Library/Logs/Unity/Editor.log $OUTPUT_DIR/unity_editor_step_4.log
+
+
+echo "Cleaning up: Put deleted script back"
+mv $(pwd)/tmp_deleted_file/TestScript_toDelete.cs* $UNITY_PROJECT_PATH/Assets/MyFolder/
